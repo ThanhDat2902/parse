6161affbaa57d47269e2ddcbec4f6c820723c4cc
Mickey Le <thanhdat2902@gmail.com>
2017-01-17 14:23:38 +0100

    multi-app

--- index.js
+++ index.js
@@ -11,31 +11,41 @@ var path = require('path');
 //  console.log('DATABASE_URI not specified, falling back to localhost.');
 //}
 
-var api = new ParseServer({
-  databaseURI: 'mongodb://user:key@ds053186.mlab.com:53186/heroku_lkggr5wx',
-  cloud: __dirname + '/cloud/main.js',
-  verbose: true,
-  appId: 'myAppId',
-  masterKey: 'key', //Add your master key here. Keep it secret!
-  serverURL: 'https://safe-basin-38488.herokuapp.com/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
-  ,push: {
+
+var api1 = new ParseServer({
+    databaseURI: 'mongodb://user:key@ds053186.mlab.com:53186/heroku_lkggr5wx',
+    //cloud: __dirname + '/cloud/main.js',
+    verbose: true,
+    appId: 'myAppId1',
+    masterKey: 'key', //Add your master key here. Keep it secret!
+    //serverURL: 'https://safe-basin-38488.herokuapp.com/parse',
+    serverURL: 'http://localhost:1337/parse',
+    liveQuery: {
+        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    }
+  , push: {
       android: {
           senderId: '423654199290', // The Sender ID of GCM
-          apiKey: 'AIzaSyDdCNomOTvNpDEzAqBE4EWomNS5BNMcrXs' // The Server API Key of GCM
-      //},
-      //ios: {
-      //    pfx: 'certs/mycert.p12', // the path and filename to the .p12 file you exported earlier. 
-      //    cert: '', // If not using the .p12 format, the path to the certificate PEM to load from disk
-      //    bundleId: '', // The bundle identifier associated with your app
-      //    key: '', // If not using the .p12 format, the path to the private key PEM to load from disk
-      //    production: true // Specifies which environment to connect to: Production (if true) or Sandbox
+          apiKey: 'AIzaSyDdCNomOTvNpDEzAqBE4EWomNS5BNMcrXs' // The Server API Key of GCM  
       }
   }
-
-
+});
+var api2 = new ParseServer({
+    databaseURI: 'mongodb://user:key@ds053186.mlab.com:53186/heroku_lkggr5wx',
+    //cloud: __dirname + '/cloud/main.js',
+    verbose: true,
+    appId: 'myAppId2',
+    masterKey: 'key', //Add your master key here. Keep it secret!
+    serverURL: 'http://localhost:1337/parse',
+    liveQuery: {
+        classNames: ["Posts", "Comments"]
+    }
+  , push: {
+      android: {
+          senderId: '1077532576696',
+          apiKey: 'AIzaSyD_6h7I5uP3FqU_iOuURjCOJl2lLMZwM2c'
+      }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -48,7 +58,8 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
-app.use(mountPath, api);
+app.use(mountPath, api1);
+app.use(mountPath,api2)
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
